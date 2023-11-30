4e47519ce0bbd8fc8045a25f7f815013fa5c881f
Arsen <arsmur85@gmail.com>
2023-11-30 17:52:47 +0300

    finished commit

--- src/index.js
+++ src/index.js
@@ -8,21 +8,3 @@ root.render(
         <Home />
     </React.StrictMode>,
 );
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Apple from './components/app/app';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Apple />
-        </BrowserRouter>
-    </React.StrictMode>,
-);
-
-*/
