92cc75fbcebc06242ae4ec0e44df0c77cdc2c73e
Arsen <arsmur85@gmail.com>
2023-12-05 15:01:58 +0300

    remember me

--- src/components/app/Login.js
+++ src/components/app/Login.js
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = ({ onLogin }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
 
-    const submitThis = (e) => {
+    const saveLoginInfo = () => {
+        const timestamp = Date.now();
+        const info = { email, password, timestamp };
+        localStorage.setItem('loginInfo', JSON.stringify(info));
+    };
+
+    useEffect(() => {
+        const storedInfo = localStorage.getItem('loginInfo');
+
+        if (storedInfo) {
+            const {
+                email: storedEmail,
+                password: storedPassword,
+                timestamp,
+            } = JSON.parse(storedInfo);
+            setEmail(storedEmail);
+            setPassword(storedPassword);
+
+            const twoHours = 2 * 60 * 60 * 1000;
+            const elapsedTime = Date.now() - timestamp;
+
+            if (elapsedTime < twoHours) {
+                onLogin({ email: storedEmail, password: storedPassword });
+            }
+        }
+    }, [onLogin]);
+
+    const submitForm = (e) => {
         e.preventDefault();
 
         const info = { email, password };
+
         onLogin(info);
+
+        if (rememberMe) {
+            saveLoginInfo();
+        } else {
+            localStorage.removeItem('loginInfo');
+        }
 
         setEmail('');
         setPassword('');
@@ -16,7 +51,7 @@ const Login = ({ onLogin }) => {
 
     return (
         <div>
-            <form onSubmit={submitThis}>
+            <form onSubmit={submitForm}>
                 <div>
                     <label htmlFor="email">Email</label>
                     <input
@@ -42,6 +77,16 @@ const Login = ({ onLogin }) => {
                 <button className="btn btn-primary" type="submit">
                     Submit
                 </button>
+
+                <label className="container">
+                    Remember Me
+                    <input
+                        type="checkbox"
+                        checked={rememberMe}
+                        onChange={() => setRememberMe(!rememberMe)}
+                    />
+                    <span className="checkmark"></span>
+                </label>
             </form>
         </div>
     );
